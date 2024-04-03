ae9082cd746f63d527e286a00a26a39c79a3da06
John Mosesman <johnmosesman@gmail.com>
2024-04-03 16:31:40 -0500

    Adjust workbook name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { FlatfileListener } from '@flatfile/listener';
-import { plmProjectSpaceConfigure } from './workflows/plm/actions/plmProjectSpaceConfigure';
+import {
+  plmProjectSpaceConfigure,
+  WORKBOOK_NAME,
+} from './workflows/plm/actions/plmProjectSpaceConfigure';
 import { ecommerceProjectSpaceConfigure } from './workflows/ecommerce/actions/ecommerceProjectSpaceConfigure';
 import { fieldServicesProjectSpaceConfigure } from './workflows/fieldServices/actions/fieldServicesProjects';
 import { ExcelExtractor } from '@flatfile/plugin-xlsx-extractor';
@@ -10,6 +13,7 @@ import { handleSubmitData } from './shared/eventHandlers/handleSubmitData';
 import api from '@flatfile/api';
 import { attributes as attributesBlueprint } from './workflows/plm/blueprints/attributes';
 import { ProductsShowApiService } from './shared/products-show-api-service';
+import { WorkbookResponse } from '@flatfile/api/api';
 
 const namespaceConfigs = {
   'space:plmproject': plmProjectSpaceConfigure,
@@ -28,13 +32,16 @@ function configureNamespace(listener: FlatfileListener, namespace: string) {
 
     // Seed the workbook with data
     listener.on('workbook:created', async (event) => {
+      console.log('=======================');
+      console.log('WORKBOOK CREATED EVENT');
+      console.log('=======================');
       if (!event.context || !event.context.workbookId) {
         console.error('Event context or workbookId missing');
         return;
       }
 
       const workbookId = event.context.workbookId;
-      let workbook;
+      let workbook: WorkbookResponse;
       try {
         workbook = await api.workbooks.get(workbookId);
       } catch (error) {
@@ -42,12 +49,10 @@ function configureNamespace(listener: FlatfileListener, namespace: string) {
         throw error;
       }
 
-      const workbookName =
-        workbook.data && workbook.data.name ? workbook.data.name : '';
+      const workbookName = workbook?.data?.name ? workbook.data.name : '';
 
-      if (workbookName.includes('HCM Workbook')) {
-        const sheets =
-          workbook.data && workbook.data.sheets ? workbook.data.sheets : [];
+      if (workbookName === WORKBOOK_NAME) {
+        const sheets = workbook?.data?.sheets ? workbook.data.sheets : [];
 
         const attributesSheet = sheets.find(
           (s) => s.config.slug === attributesBlueprint.slug
@@ -76,6 +81,8 @@ function configureNamespace(listener: FlatfileListener, namespace: string) {
             console.error('Error inserting attributes:', error.message);
           }
         }
+      } else {
+        console.error('Error - no workbook found');
       }
     });
 
@@ -93,8 +100,7 @@ function configureNamespace(listener: FlatfileListener, namespace: string) {
 export default function (listener: FlatfileListener) {
   listener.on('**', (event) => {
     console.log(
-      `-> My event listener received an event: 
-      ${JSON.stringify(event.topic)}
+      `[${JSON.stringify(event.topic)}]
       ${JSON.stringify(event.namespace)} 
       ${JSON.stringify(event.payload)}`
     );
