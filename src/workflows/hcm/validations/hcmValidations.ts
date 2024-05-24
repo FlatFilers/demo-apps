d75e5903f255da01a513f409f9f0078b37e35acc
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-05-24 17:12:00 -0600

    Fix job validations

--- src/workflows/hcm/validations/hcmValidations.ts
+++ src/workflows/hcm/validations/hcmValidations.ts
@@ -1,11 +1,20 @@
 import { bulkRecordHook } from '@flatfile/plugin-record-hook';
 import { FlatfileListener } from '@flatfile/listener';
 import { employeeValidations } from '@/workflows/hcm/validations/employeeValidations';
+import { jobValidations } from '@/workflows/hcm/validations/jobValidations';
 
 export async function hcmValidations(listener: FlatfileListener) {
   listener.use(
     bulkRecordHook('employees-sheet', async (records, event) => {
       await employeeValidations(records, event);
+    })
+  );
+
+  listener.use(
+    bulkRecordHook('jobs-sheet', async (records) => {
+      records.forEach((record) => {
+        jobValidations(record);
+      });
     })
   );
 }
